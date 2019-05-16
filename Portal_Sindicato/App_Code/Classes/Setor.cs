8c1312d4d4ad11ea299f06b28641e012c137dc6a
KAUE SILVA <kaue.silva10@fatec.sp.gov.br>
2019-05-16 19:36:51 -0300

    Commit 3 Kauê

---
+++ Portal_Sindicato/App_Code/Classes/Setor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Descrição resumida de Setor
+/// </summary>
+public class Setor
+{
+    public Setor()
+    {
+        public string tipo { get; set; }
+        
+    //
+    // TODO: Adicionar lógica do construtor aqui
+    //
+}
+}
