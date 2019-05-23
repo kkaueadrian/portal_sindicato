ee93a26968e39ef077e7dd5625ca7d34ed0a092d
KAUE SILVA <kaue.silva10@fatec.sp.gov.br>
2019-05-23 20:56:17 -0300

    Inserção da SindicatoSetor e SindicatoSetorBD

---
+++ Portal_Sindicato/App_Code/Classes/SindicatoSetor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace classes
+{
+    /// <summary>
+    /// Descrição resumida de Sindicato
+    /// </summary>
+    public class SindicatoSetor
+    {
+
+        public Sindicato Sindicato { get; set; }
+        public Setor Setor { get; set; }
+
+        public SindicatoSetor()
+        {
+
+        }
+    }
+}
