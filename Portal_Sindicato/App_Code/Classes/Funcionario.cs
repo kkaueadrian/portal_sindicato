4a5a53b370bda1e70edd076e03699f97d7966183
JOSE BARRA <jose.barra@fatec.sp.gov.br>
2019-05-23 00:11:04 -0300

    Fun

--- Portal_Sindicato/App_Code/Classes/Funcionario.cs
+++ Portal_Sindicato/App_Code/Classes/Funcionario.cs
@@ -12,16 +12,8 @@ namespace classes
     public class Funcionario:Pessoa
     {
         //propriedades
-        public int Codigo { get; set; }
+        
         public Boolean PermissaoAdministrador { get; set; }
-        public string Nome { get; set; }
-        public string Senha { get; set; }
-        public string Cpf { get; set; }
-        public DateTime Nascimento { get; set; }
-        public DateTime Dataadm { get; set; }
-        public string Endereco { get; set; }
-        public string Email { get; set; }
-        public string Celular { get; set; }
         public Sindicato Sindicato { get; set; }
         public Setor Setor { get; set; }
         public int Tipo { get; set; }
