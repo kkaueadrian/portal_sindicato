91bec0d9a4167b14149fcece1c451877540cc702
pl_ta <pl_ta@DESKTOP-M6OS52H>
2019-05-29 23:18:35 -0300

    Alteracao servicos

--- Portal_Sindicato/App_Code/Persistencia/ServicoBD.cs
+++ Portal_Sindicato/App_Code/Persistencia/ServicoBD.cs
@@ -18,14 +18,14 @@ namespace persistencia
         {
             System.Data.IDbConnection objConexao;
             System.Data.IDbCommand objCommand;
-            string sql = "INSERT INTO ser_servico(ser_descricao, ser_status, ser_tipo, set_codigo, sin_codigo) VALUES ( ?descricao, ?status, ?tipo, ?setor, ?sindicato)";
+            string sql = "INSERT INTO ser_servico(ser_descricao, ser_status, ser_tipo, set_codigo) VALUES ( ?descricao, ?status, ?tipo, ?setor)";
             objConexao = Mapped.Connection();
             objCommand = Mapped.Command(sql, objConexao);
             objCommand.Parameters.Add(Mapped.Parameter("?descricao", servico.Descricao));
             objCommand.Parameters.Add(Mapped.Parameter("?status", servico.Status));
             objCommand.Parameters.Add(Mapped.Parameter("?tipo", servico.Tipo));
             objCommand.Parameters.Add(Mapped.Parameter("?setor", servico.Setor.Codigo));
-            objCommand.Parameters.Add(Mapped.Parameter("?sindicato", servico.Sindicato.Codigo));
+           
             objCommand.ExecuteNonQuery();
             objConexao.Close();
             objCommand.Dispose();
@@ -48,14 +48,14 @@ namespace persistencia
             objConexao.Dispose();
             return ds;
         }
-        public DataSet SelectAllWithsectorandsindicate()
+        public DataSet SelectAllWithsector()
         {
             DataSet ds = new DataSet();
             System.Data.IDbConnection objConexao;
             System.Data.IDbCommand objCommand;
             System.Data.IDataAdapter objDataAdapter;
             objConexao = Mapped.Connection();
-            objCommand = Mapped.Command("SELECT * FROM ser_servico s inner join sin_sindicato si on s.sin_codigo = si.sin_codigo inner join set_setor se on s.set_codigo = se.set_codigo order by ser_codigo", objConexao);
+            objCommand = Mapped.Command("SELECT * FROM ser_servico s inner join set_setor se on s.set_codigo = se.set_codigo order by ser_codigo", objConexao);
             objDataAdapter = Mapped.Adapter(objCommand);
             objDataAdapter.Fill(ds);
             objConexao.Close();
