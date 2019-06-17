7420b5ae443ab6fc186feaadffe12f560f872dce
JOSE BARRA <jose.barra@fatec.sp.gov.br>
2019-06-17 15:32:33 -0300

    login classe

---
+++ Portal_Sindicato/App_Code/Classes/Login.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace classes
+
+
+{
+
+    public class Login
+    {
+        public int Codigo { get; set; }
+        public DateTime Tempo { get; set; }
+        public Pessoa Pessoa { get; set; }
+
+    public Login()
+    {
+        //
+        // TODO: Adicionar lógica do construtor aqui
+        //
+    }
+}
+
+}
---
+++ Portal_Sindicato/App_Code/Persistencia/LoginBD.cs
@@ -0,0 +1,38 @@
+﻿using FATEC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI.WebControls;
+
+namespace persistencia
+{
+
+    public class LoginBD
+    {
+
+        public bool Insert(Login login)
+        {
+            System.Data.IDbConnection objConexao;
+            System.Data.IDbCommand objCommand;
+            string sql = "INSERT INTO ser_servico(ser_descricao, ser_status, ser_tipo, set_codigo) VALUES ( ?descricao, ?status, ?tipo, ?setor)";
+            objConexao = Mapped.Connection();
+            objCommand = Mapped.Command(sql, objConexao);
+            objCommand.Parameters.Add(Mapped.Parameter("?descricao", login.Tempo));
+            objCommand.Parameters.Add(Mapped.Parameter("?setor", login.Pessoa.Codigo));
+
+            objCommand.ExecuteNonQuery();
+            objConexao.Close();
+            objCommand.Dispose();
+            objConexao.Dispose();
+            return true;
+        }
+
+        public LoginBD()
+        {
+            //
+            // TODO: Adicionar lógica do construtor aqui
+            //
+        }
+    }
+}
